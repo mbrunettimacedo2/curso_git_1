0adcc4b23337032d1a1bb293cfd37ca16e5aae7e
Matheus Brunetti Macedo <mbmacedo2005@gmail.com>
2025-02-11 12:27:39 -0300

    Enviando a funcionalidade X

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        String nome = "mathus";
+        System.out.println(nome);
+        
+    }
+    
+   
+    
+}
