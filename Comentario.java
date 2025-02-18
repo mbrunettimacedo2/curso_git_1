823c23f3de583c91fc920a19c23c415e5023dc83
Matheus Brunetti Macedo <mbmacedo2005@gmail.com>
2025-02-18 12:07:28 -0300

    Hello World

---
+++ Comentario.java
@@ -0,0 +1,55 @@
+public class Comentario {
+    // Java
+
+    /*oiiii */
+
+    /** 
+     * 1
+     * 2
+     * 3
+     * 4
+     * comentário para documentação */
+public static void main(String[] args){
+     String firstName = "Matheus";
+     String secondName = "Macedo";
+     String fullName = firstName + " " + secondName;
+     String full = "O nome é: " + fullName;
+
+     System.out.println(firstName + secondName);
+     System.out.println(fullName);
+     System.out.println(full);
+
+     char letra = 'A';
+     System.out.println(letra);
+
+     char simbolo = '$';
+     System.out.println(simbolo);
+
+     int numeroFixo = 560;
+     System.out.println(numeroFixo);
+
+    int n = 50;
+    System.out.println(n + 50);
+
+    System.out.println(n * 3);
+
+    System.out.println(n / 3);
+
+    long numeroGrandao = 500_000_000_000L;
+    long numeroGrande = 500000000000L;
+
+    System.out.println(numeroGrandao + 1);
+
+    double numeroComVirgula = 19.99D;
+
+    double preco = 77.890d;
+
+    double precoBaixo = 12.99;
+    System.out.println(numeroComVirgula);
+    System.out.println(preco);
+    System.out.println(precoBaixo);
+
+    double pi = 3.14_156_123;
+    System.out.println(pi);
+    }
+}
